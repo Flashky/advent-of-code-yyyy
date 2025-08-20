8bd7f2487d3c2152384ec0ba3091db39d5a90f14
Flashky <mevengar@gmail.com>
2025-08-20 13:21:30 +0200

    feat: add summation functions (beta)

--- src/main/java/com/adventofcode/flashk/common/MathUtil.java
+++ src/main/java/com/adventofcode/flashk/common/MathUtil.java
@@ -1,5 +1,8 @@
 package com.adventofcode.flashk.common;
 
+import java.util.function.DoubleFunction;
+import java.util.function.LongFunction;
+
 public final class MathUtil {
 
 	private MathUtil() {}
@@ -31,4 +34,62 @@ public final class MathUtil {
 		
 		return value + summation(value-1);
 	}
+
+    /**
+     * Applies the sums of a function over a range of values.
+     * Example:
+     * <pre>
+     *     summation(1, 5, i -> i * i) // returns 55 (1^2 + 2^2 + 3^2 + 4^2 + 5^2)
+     *     summation(1, 5, i -> i) // returns 15 (1 + 2 + 3 + 4 + 5)
+     *     summation(0, 0, i -> i) // returns 0 (0)
+     * </pre>
+     * @param min the minimum value to start the summation. Must be smaller or equal to max.
+     * @param max the maximum value to end the summation. Must be greater or equal to min.
+     * @param summationFunction the function to apply to each value in the range before summing them up.
+     * @return the summation of the function applied to each value in the range
+     * @throws IllegalArgumentException if min is greater than max
+     */
+    public static long summation(long min, long max, LongFunction<Long> summationFunction) {
+
+        if(min > max) {
+            throw new IllegalArgumentException("min must be less than or equal to max");
+        }
+
+        long sum = 0;
+
+        for (long i = min; i <= max; i++) {
+            sum += summationFunction.apply(i);
+        }
+
+        return sum;
+    }
+
+    /**
+     * Applies the sums of a function over a range of values.
+     * Example:
+     * <pre>
+     *     summation(1, 5, i -> i * i) // returns 55 (1^2 + 2^2 + 3^2 + 4^2 + 5^2)
+     *     summation(1, 5, i -> i) // returns 15 (1 + 2 + 3 + 4 + 5)
+     *     summation(0, 0, i -> i) // returns 0 (0)
+     * </pre>
+     * @param min the minimum value to start the summation. Must be smaller or equal to max.
+     * @param max the maximum value to end the summation. Must be greater or equal to min.
+     * @param summationFunction the function to apply to each value in the range before summing them up.
+     * @return the summation of the function applied to each value in the range
+     * @throws IllegalArgumentException if min is greater than max
+     */
+    public static double summation(double min, double max, DoubleFunction<Double> summationFunction) {
+
+        if(min > max) {
+            throw new IllegalArgumentException("min must be less than or equal to max");
+        }
+
+        double sum = 0;
+
+        for (double i = min; i <= max; i++) {
+            sum += summationFunction.apply(i);
+        }
+
+        return sum;
+    }
 }
--- src/test/java/com/adventofcode/flashk/common/MathUtilTest.java
+++ src/test/java/com/adventofcode/flashk/common/MathUtilTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.function.DoubleFunction;
+import java.util.function.LongFunction;
+
 class MathUtilTest {
 
 	@BeforeAll
@@ -35,4 +38,48 @@ class MathUtilTest {
 	void testSummation10() {
 		assertEquals(55, MathUtil.summation(10));
 	}
+
+    @Test
+    void testSummationWithLongFunction() {
+
+        // Example function: f(i) = i
+        LongFunction<Long> function = i -> i;
+        assertEquals(0, MathUtil.summation(0, 0, function));
+        assertEquals(1, MathUtil.summation(1, 1, function));
+        assertEquals(15, MathUtil.summation(1, 5, function));
+
+        // Example function: f(i) = i^2
+        function = i -> i * i;
+        assertEquals(55, MathUtil.summation(1, 5, function));
+
+        // Example function: f(i) = (i-3)^2
+        function = i -> (long) Math.pow(i-3, 2);
+        assertEquals(2567900, MathUtil.summation(1, 200, function));
+
+        // Example function: f(i) = i^3 - i^2
+        function = i -> (long) Math.pow(i,3) - (long) Math.pow(i,2);
+        assertEquals(350, MathUtil.summation(1, 6, function));
+    }
+
+    @Test
+    void testSummationWithDoubleFunction() {
+
+        // Example function: f(i) = i
+        DoubleFunction<Double> function = i -> i;
+        assertEquals(0, MathUtil.summation(0, 0, function));
+        assertEquals(1, MathUtil.summation(1, 1, function));
+        assertEquals(15, MathUtil.summation(1, 5, function));
+
+        // Example function: f(i) = i^2
+        function = i -> i * i;
+        assertEquals(55, MathUtil.summation(1, 5, function));
+
+        // Example function: f(i) = (i-3)^2
+        function = i -> Math.pow(i-3, 2);
+        assertEquals(2567900, MathUtil.summation(1, 200, function));
+
+        // Example function: f(i) = i^3 - i^2
+        function = i -> Math.pow(i,3) - Math.pow(i,2);
+        assertEquals(350, MathUtil.summation(1, 6, function));
+    }
 }
