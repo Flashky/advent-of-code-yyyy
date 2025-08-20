ce14a4f64d2c692de6f6bd7225f96a6c991c52e1
Flashk <mevengar@gmail.com>
2025-08-20 19:59:40 +0200

    feat: add summation method for integer values

--- src/main/java/com/adventofcode/flashk/common/MathUtil.java
+++ src/main/java/com/adventofcode/flashk/common/MathUtil.java
@@ -1,11 +1,41 @@
 package com.adventofcode.flashk.common;
 
 import java.util.function.DoubleFunction;
+import java.util.function.IntFunction;
 import java.util.function.LongFunction;
 
 public final class MathUtil {
 
 	private MathUtil() {}
+
+	/**
+	 * Applies the sums of a function over a range of values.
+	 * Example:
+	 * <pre>
+	 *     summation(1, 5, i -> i * i) // returns 55 (1^2 + 2^2 + 3^2 + 4^2 + 5^2)
+	 *     summation(1, 5, i -> i) // returns 15 (1 + 2 + 3 + 4 + 5)
+	 *     summation(0, 0, i -> i) // returns 0 (0)
+	 * </pre>
+	 * @param min the minimum value to start the summation. Must be smaller or equal to max.
+	 * @param max the maximum value to end the summation. Must be greater or equal to min.
+	 * @param summationFunction the function to apply to each value in the range before summing them up.
+	 * @return the summation of the function applied to each value in the range
+	 * @throws IllegalArgumentException if min is greater than max
+	 */
+	public static int summation(int min, int max, IntFunction<Integer> summationFunction) {
+
+		if(min > max) {
+			throw new IllegalArgumentException("min must be less than or equal to max");
+		}
+
+		int sum = 0;
+
+		for (int i = min; i <= max; i++) {
+			sum += summationFunction.apply(i);
+		}
+
+		return sum;
+	}
 
     /**
      * Applies the sums of a function over a range of values.
--- src/test/java/com/adventofcode/flashk/common/MathUtilTest.java
+++ src/test/java/com/adventofcode/flashk/common/MathUtilTest.java
@@ -2,22 +2,36 @@ package com.adventofcode.flashk.common;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.function.DoubleFunction;
+import java.util.function.IntFunction;
 import java.util.function.LongFunction;
 
 class MathUtilTest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+    @Test
+    void testSummationWithIntFunction() {
+
+        // Example function: f(i) = i
+        IntFunction<Integer> function = i -> i;
+        assertEquals(0, MathUtil.summation(0, 0, function));
+        assertEquals(1, MathUtil.summation(1, 1, function));
+        assertEquals(15, MathUtil.summation(1, 5, function));
+
+        // Example function: f(i) = i^2
+        function = i -> i * i;
+        assertEquals(55, MathUtil.summation(1, 5, function));
+
+        // Example function: f(i) = (i-3)^2
+        function = i -> (int) Math.pow(i-3, 2);
+        assertEquals(2567900, MathUtil.summation(1, 200, function));
+
+        // Example function: f(i) = i^3 - i^2
+        function = i -> (int) Math.pow(i,3) - (int) Math.pow(i,2);
+        assertEquals(350, MathUtil.summation(1, 6, function));
+    }
 
     @Test
     void testSummationWithLongFunction() {
